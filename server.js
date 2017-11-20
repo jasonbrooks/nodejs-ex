ad629bd7e387328ea1f0b398969b01840096ab11
David Martin <david.martin@redhat.com>
2017-11-20 16:11:25 +0000

    ping endpoint

--- server.js
+++ server.js
@@ -89,6 +89,10 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/ping', function (req, res) {
+  res.send('{"status":"OK"}');
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
